d9431134019f2956b865296e1c0fb7de926c2682
kanikaprabhakar <kanikaprabhakar2005@gmail.com>
2026-05-23 22:01:58 +0530

    removed it all

--- server/routes/ai.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import * as aiController from '../controllers/aiController.js';
-
-const router = express.Router();
-
-router.post('/chat', aiController.chatWithAI);
-
-export default router;
